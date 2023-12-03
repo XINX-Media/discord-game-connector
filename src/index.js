f7f8d4b0be5421011a1b4af2cd5fd75b7b10011b
Michael <michael@studiomav.com>
2023-12-02 17:19:37 -0800

    basic discord client and auth

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-import 'dotenv/config';
-export * from './Foo';
-export { default as Foo } from './Foo';
+require('dotenv').config()
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { token } = process.env.DISCORD_TOKEN;
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code only once
+client.once(Events.ClientReady, readyClient => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+// Log in to Discord with your client's token
+client.login(token);
