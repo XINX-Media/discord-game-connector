79345fbd208199564374e618411e6a13ca175ba4
Michael <michael@studiomav.com>
2024-01-29 21:08:12 -0800

    updated class structure and included test

--- src/ViewerConnection.js
+++ src/ViewerConnection.js
@@ -1,64 +1,72 @@
 require('dotenv').config();
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 
-class ViewerConnection
-{
-	constructor([commands])
-	{
-		
+class ViewerConnection {
+	constructor(commands) {
+		this.commands = commands;
 	}
 
-	connect()
-	{
-		
-	}
-
-	handleMessage()
-	{
+	connect() {
 
 	}
 
-	sendMessage(message)
-	{
+	handleMessage() {
+
+	}
+
+	sendMessage(message) {
 
 	}
 }
 
-class DiscordConnection extends ViewerConnection
-{
-	constructor([commands])
-	{
-
+class DiscordConnection extends ViewerConnection {
+	constructor() {
+		super(commands);
 	}
 
-	connect()
-	{
-		const { token } = process.env.DISCORD_TOKEN;
+	connect() {
+		return new Promise((resolve) => {
+			const { token } = process.env.DISCORD_TOKEN;
 
-		// Create a new client instance
-		const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+			// Create a new client instance
+			this.client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-		// When the client is ready, run this code only once
-		client.once(Events.ClientReady, readyClient => {
-			console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+			// When the client is ready, run this code only once
+			this.client.once(Events.ClientReady, readyClient => {
+				console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+				resolve();
+			});
+
+			// Log in to Discord with your client's token
+			this.client.login(token);
 		});
-
-		// Log in to Discord with your client's token
-		client.login(token);
 	}
 
-	sendMessage(channel, messageContent)
-	{
-		channel.send(
-		{
-			message: "Message text",
-			files: [{
-			  attachment: 'example.file',
-			  name: 'filename',
-			  description: 'file description for accessibility'
-			}]
-		})
+	sendMessage(channelID, messageContent) {
+		const channel = this.client.channels.cache.get(channelID);
+		channel.send(messageContent)
 			.then(console.log)
 			.catch(console.error);
+			/*
+			{
+				message: "Message text",
+				files: [{
+					attachment: 'example.file',
+					name: 'filename',
+					description: 'file description for accessibility'
+				}]
+			}
+			*/
 	}
 }
+
+const commands =
+	[
+
+	];
+
+const discordConnection = new DiscordConnection(commands);
+discordConnection.connect().then(() => 
+{
+	discordConnection.sendMessage(process.env.CHANNEL_ID, 'test');
+});
